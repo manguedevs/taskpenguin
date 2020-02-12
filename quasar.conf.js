2cd9356070688ce4914d0a3655802d1f7ee48be7
brunocordioli <brunocordioli072@gmail.com>
2020-02-12 01:29:29 -0300

    Login completo com auth0 done.

--- quasar.conf.js
+++ quasar.conf.js
@@ -48,7 +48,7 @@ module.exports = function (ctx) {
       directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: [ 'Notify' ]
     },
 
     // https://quasar.dev/quasar-cli/cli-documentation/supporting-ie
@@ -82,7 +82,7 @@ module.exports = function (ctx) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
     devServer: {
       https: false,
-      port: 8080,
+      port: 3000,
       open: true // opens browser window automatically
     },
 
