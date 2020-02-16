37f0771124d5a17664f47531134b349f87795385
brunocordioli <brunocordioli072@gmail.com>
2020-02-16 18:13:42 -0300

    Apresentar dados do auth apos login no drawer done.

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,102 +1,138 @@
 <template>
-    <div>
-        <q-drawer
-            show-if-above
-            :width="250"
-            :breakpoint="0"
+  <div>
+    <q-drawer show-if-above :width="250" :breakpoint="0">
+      <q-list
+        exact-active-class="activeLink"
+        class="items"
+        style="background-color:#191F28;height: 100%;border-right: 1px solid #bbb; color:#B6B6B7"
+      >
+        <q-item class="q-pt-md q-pb-md" style="color:white">
+          <div class="row" style="margin-left:-0.5vw; height:100px">
+            <div class="col-12 text-center">
+              <span class="loginTitle drawerTitle" style>
+                <strong>TASK</strong>
+                PENGUIN {{ color }}
+              </span>
+              <br />
+              <q-avatar size="84px" color="teal-2">
+                <img src="https://static.thenounproject.com/png/48550-200.png" />
+              </q-avatar>
+            </div>
+            <div class="text-center q-pt-sm text-subtitle1 full-width">
+                <b>{{ user.nickname }}</b>
+                <br />
+                {{ user.name }}
+            </div>
+          </div>
+        </q-item>
+
+        <q-item
+          active-class="activeLink"
+          class="drawerItems row q-mt-xl q-pt-md"
+          v-ripple
+          to="/home"
+          style=" margin-top: 70px"
+          clickable
         >
-            <q-list exact-active-class="activeLink" class="items" style="background-color:#191F28;height: 100%;border-right: 1px solid #bbb; color:#B6B6B7">
-                <q-item class="q-pt-md q-pb-md" style="color:white">
-                    <div class="row" style="margin-left:-0.5vw; height:100px">
-                        
-                        <div class="col-12 text-center">
-                            <span class="loginTitle drawerTitle" style="">
-                                <strong> TASK</strong>PENGUIN {{ color }}
-                            </span>
-                        <br>
-                        <q-avatar size="84px" color="teal-2">
-                        <img
-                            src="https://static.thenounproject.com/png/48550-200.png"
-                        />
-                        </q-avatar>
-                        </div>
-                        <div class=" q-ml-md text-center text-subtitle1">
-                            <b>Jorge Ogrande</b>
-                            jorge@ogrande.com
-                        </div>
-                    </div>
-                </q-item>
-                
-                <q-item active-class="activeLink" class="drawerItems row q-mt-xl q-pt-md"  v-ripple to="/home" style=" margin-top: 70px" v-ripple clickable>
-                    <span class="q-ml-md" >
-                        <q-icon class="q-mr-sm" name="fas fa-chart-bar" /> 
-                        Informações 
-                    </span> 
-                </q-item>
+          <span class="q-ml-md">
+            <q-icon class="q-mr-sm" name="fas fa-chart-bar" />Informações
+          </span>
+        </q-item>
 
-                <q-item exact-active-class="activeLink" active-class="default" class="drawerItems row q-pt-md"  v-ripple to="/bills" clickable>
-                    <span class="q-ml-md" >
-                        <q-icon class="q-mr-sm" name="fas fa-envelope" /> 
-                        Lista de faturas 
-                    </span> 
-                </q-item>
+        <q-item
+          exact-active-class="activeLink"
+          active-class="default"
+          class="drawerItems row q-pt-md"
+          v-ripple
+          to="/bills"
+          clickable
+        >
+          <span class="q-ml-md">
+            <q-icon class="q-mr-sm" name="fas fa-envelope" />Lista de faturas
+          </span>
+        </q-item>
 
-                <q-item active-class="activeLink" class="drawerItems row q-pt-md"  v-ripple to="/bills/report" clickable>
-                    <span class="q-ml-md" >
-                        <q-icon style="" class="q-mr-sm" name="fas fa-file-export" /> 
-                        Extrato
-                    </span> 
-                </q-item>
+        <q-item
+          active-class="activeLink"
+          class="drawerItems row q-pt-md"
+          v-ripple
+          to="/bills/report"
+          clickable
+        >
+          <span class="q-ml-md">
+            <q-icon style class="q-mr-sm" name="fas fa-file-export" />Extrato
+          </span>
+        </q-item>
 
-                <q-item active-class="activeLink" class="drawerItems row q-pt-md"  v-ripple to="/account" clickable>
-                    <span class="q-ml-md" >
-                        <q-icon class="q-mr-sm" name="fas fa-user-cog" /> 
-                        Dados cadastrais
-                    </span> 
-                </q-item>
+        <q-item
+          active-class="activeLink"
+          class="drawerItems row q-pt-md"
+          v-ripple
+          to="/account"
+          clickable
+        >
+          <span class="q-ml-md">
+            <q-icon class="q-mr-sm" name="fas fa-user-cog" />Dados cadastrais
+          </span>
+        </q-item>
 
-                <q-item active-class="activeLink" class="drawerItems row q-pt-md"  v-ripple to="/faq" clickable>
-                    <span class="q-ml-md" >
-                        <q-icon class="q-mr-sm" name="fas fa-question-circle" /> 
-                        FAQ
-                    </span> 
-                </q-item>
+        <q-item
+          active-class="activeLink"
+          class="drawerItems row q-pt-md"
+          v-ripple
+          to="/faq"
+          clickable
+        >
+          <span class="q-ml-md">
+            <q-icon class="q-mr-sm" name="fas fa-question-circle" />FAQ
+          </span>
+        </q-item>
 
-                <q-item active-class="activeLink" class="drawerItems absolute-bottom row q-pt-md"  v-ripple @click="logout" clickable>
-                    <span class="q-ml-md" >
-                        <q-icon class="q-mr-sm" name="fas fa-sign-out-alt" style="transform: rotate(180deg)"/> 
-                        Sign out
-                    </span> 
-                </q-item>
-
-            </q-list>
-        </q-drawer>
-    </div>
+        <q-item
+          active-class="activeLink"
+          class="drawerItems absolute-bottom row q-pt-md"
+          v-ripple
+          @click="logout"
+          clickable
+        >
+          <span class="q-ml-md">
+            <q-icon class="q-mr-sm" name="fas fa-sign-out-alt" style="transform: rotate(180deg)" />Sign out
+          </span>
+        </q-item>
+      </q-list>
+    </q-drawer>
+  </div>
 </template>
 
 <script>
-
 export default {
-    methods:{
-        logout(){
-            this.$auth.logout();
-        },
+  data() {
+    return {
+      user: this.$auth.user
+    };
+  },
+  methods: {
+    logout() {
+      this.$auth.logout();
     }
-
-}
+  }
+};
 </script>
 
 <style lang="scss">
-.drawerTitle{
-    font-size:22px;
-    background: linear-gradient(90deg, rgba(255,184,140,0.8) 30%, rgba(229,101,144,1) 75%);
-    background-clip: text;
-    -webkit-text-fill-color: transparent;
-
+.drawerTitle {
+  font-size: 22px;
+  background: linear-gradient(
+    90deg,
+    rgba(255, 184, 140, 0.8) 30%,
+    rgba(229, 101, 144, 1) 75%
+  );
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
 }
 
-.drawerItems{
-    font-size:15px; 
-    height:50px
+.drawerItems {
+  font-size: 15px;
+  height: 50px;
 }
 </style>
